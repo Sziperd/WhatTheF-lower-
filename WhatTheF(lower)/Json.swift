ec1e226f3ffe1d3be19be36f75d02ec98c4ce862
Patryk Piwowarczyk <shepherd90@windowslive.com>
2022-03-14 11:36:07 +0100

    Added wikipedia API fetching information about a flower that has been detected by the MLmodel

---
+++ WhatTheF(lower)/Json.swift
@@ -0,0 +1,12 @@
+//
+//  Json.swift
+//  WhatTheF(lower)
+//
+//  Created by Patryk Piwowarczyk on 14/03/2022.
+//
+
+import Foundation
+
+
+
+
--- WhatTheF(lower)/ViewController.swift
+++ WhatTheF(lower)/ViewController.swift
@@ -6,14 +6,133 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+import Alamofire
+import SwiftyJSON
+import ColorThiefSwift
+import SDWebImage
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var label: UILabel!
+    
+    let wikipediaURL = "https://en.wikipedia.org/w/api.php"
+    
+    let imagePicker = UIImagePickerController()
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
         // Do any additional setup after loading the view.
     }
 
+    
+    func requestInfo(flowerName: String){
+        let parameters : [String:String] = [
+            "format" : "json",
+            "action" : "query",
+            "prop"   : "extracts",
+            "exintro": "",
+            "explaintext" : "",
+            "titles" : flowerName,
+            "indexpageids" : "",
+            "redirects" : "1",
 
+
+        ]
+        AF.request(wikipediaURL, method: .get, parameters: parameters).responseJSON { (response) in
+        switch response.result {
+        case .success(let value):
+
+        print("got the wikipedia info")
+        print(response.result)
+
+        let flowerJSON: JSON = JSON(value)
+
+        let pageid = flowerJSON["query"]["pageids"][0].stringValue
+
+        let flowerDescription = flowerJSON["query"]["pages"][pageid]["extract"].stringValue
+
+        print(flowerDescription)
+
+        self.label.text = flowerDescription
+
+        case .failure:
+        print("did not get the wikipedia info")
+
+
+        }
+        }
+        
+     
+        
+    }
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+        
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+        self.imageView.image = userPickedImage
+            
+            guard  let ciimage = CIImage(image: userPickedImage) else{
+                fatalError()
+            }
+            detect(image: ciimage)
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+        
+        
+    }
+    
+    func detect(image: CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else{
+            fatalError()
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError()
+            }
+            
+            
+            
+            
+            if let firstResult = results.first{
+                print(results)
+                self.navigationItem.title =  firstResult.identifier
+                self.requestInfo(flowerName: firstResult.identifier)
+               
+            }
+            
+            
+            
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        
+        do {
+        try handler.perform([request])
+        }catch {
+            print(error)
+        }
+    }
+    
 }
 
